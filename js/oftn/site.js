8ce498150a743849228552b18e8e11b82d650a5d
Eli Grey <me@eligrey.com>
2013-05-12 02:37:15 -0400

    Updating site default JS.

--- js/oftn/site.js
+++ js/oftn/site.js
@@ -1,19 +1,17 @@
 "use strict";
 
-(function() {
-	/*
-	 * Use "latin capital letter o with stroke" in the title due to most
-	 * operating systems having bad font support for the empty set character.
-	 */
-	if (!/bot\b/.test(navigator.userAgent)) {
-		document.title = document.title.replace(/∅/g, "Ø");
-	}
+/*
+ * Use "latin capital letter o with stroke" in the title due to most
+ * operating systems having bad font support for the empty set character.
+ */
+if (!/bot\b/.test(navigator.userAgent)) {
+	document.title = document.title.replace(/∅/g, "Ø");
+}
 
-	/*
-	 * Add Google Analytics' tracking script
-	 */
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-20491376-1']);
-	_gaq.push(['_setDomainName', 'oftn.org']);
-	_gaq.push(['_trackPageview']);
-})();
+/*
+ * Add Google Analytics' tracking script
+ */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-20491376-1']);
+_gaq.push(['_setDomainName', 'oftn.org']);
+_gaq.push(['_trackPageview']);
