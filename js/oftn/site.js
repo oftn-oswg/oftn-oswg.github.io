898e55c3304aae0900a6df6944c64c941c452bfa
Devin Samarin <eboyjr14@gmail.com>
2013-06-02 08:40:50 -0700

    Move inline scripts out of head to allow parallel fetching of resources; update description

--- js/oftn/site.js
+++ js/oftn/site.js
@@ -7,3 +7,7 @@
 if (!/bot\b/.test(navigator.userAgent)) {
 	document.title = document.title.replace(/∅/g, "Ø");
 }
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-20491376-1');
+ga('send', 'pageview');
