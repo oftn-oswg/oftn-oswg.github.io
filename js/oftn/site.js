ffa9d7331e0097a631a3c98f6663f817c7cebf68
Eli Grey <~@eligrey.com>
2018-01-13 03:16:26 -0800

    Add emoji favicons

--- js/oftn/site.js
+++ js/oftn/site.js
@@ -11,3 +11,16 @@ if (!/bot\b/.test(navigator.userAgent)) {
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-20491376-1');
 ga('send', 'pageview');
+
+(function() { // randomized per-tab emoji favicons
+	var favicon_emoji = "👨‍🔬 👩‍🔬 👨‍💻 👩‍💻 👨‍🚀 👩‍🚀 🤯 🤖 ⚡ 🌧️ 🚀 💻 🧠 👻 📜 ☄️ 🛸 🌌 🌠 🛰️ ✨ 🔥".split(" ");
+	if (navigator.platform === "Win32") { // only Microsoft implements hacker cat
+		favicon_emoji.push("🐱‍💻");
+	}
+	var favicon = sessionStorage.favicon = sessionStorage.favicon || favicon_emoji[crypto.getRandomValues(new Uint8Array(1))[0] % favicon_emoji.length];
+	addEventListener("load", function() {
+		if (typeof set_emoji_favicon !== "undefined") {
+			set_emoji_favicon(favicon);
+		}
+	});
+}());
